9b4350a14b416f6564607c3b587f96b7fa9dbdaa
ledannyang <danny27995@gmail.com>
2019-06-15 16:53:30 +0200

    Backarrow color changed on ChatActivity (Theme added for this purpose)

--- app/src/main/java/com/example/tfm/activity/ChatActivity.kt
+++ app/src/main/java/com/example/tfm/activity/ChatActivity.kt
@@ -3,7 +3,10 @@ package com.example.tfm.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
 import com.example.tfm.R
+import org.jetbrains.anko.toast
 
 
 class ChatActivity : AppCompatActivity() {
@@ -26,4 +29,24 @@ class ChatActivity : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.chat_menu, menu)
+//        return true
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem?) = when(item?.itemId) {
+//
+//        R.id.videocall -> {
+//            toast("Videocalling...")
+//            true
+//        }
+//
+//        R.id.call -> {
+//            toast("Phonecalling...")
+//            true
+//        }
+//
+//        else -> super.onOptionsItemSelected(item)
+//    }
 }
