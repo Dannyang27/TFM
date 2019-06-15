917e1f8f2355e15b761baf8d2962de56ae11094e
ledannyang <danny27995@gmail.com>
2019-06-15 13:41:46 +0200

    BackArrow added on ChatActivity

--- app/src/main/java/com/example/tfm/activity/ChatActivity.kt
+++ app/src/main/java/com/example/tfm/activity/ChatActivity.kt
@@ -2,12 +2,28 @@ package com.example.tfm.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import com.example.tfm.R
+
 
 class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val toolbar = findViewById<Toolbar>(R.id.chat_toolbar)
+        setSupportActionBar(toolbar)
+        displayBackArrow()
+    }
+
+    private fun displayBackArrow(){
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
