9f8bdbdc0434b766c00b74311372d90a0e61d642
ledannyang <danny27995@gmail.com>
2019-08-24 19:24:49 +0200

    Gradle update

--- app/src/main/java/com/example/tfm/viewHolder/UserSearchViewHolder.kt
+++ app/src/main/java/com/example/tfm/viewHolder/UserSearchViewHolder.kt
@@ -5,9 +5,16 @@ import android.widget.ImageView
 import androidx.emoji.widget.EmojiTextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tfm.R
+import org.jetbrains.anko.toast
 
 class UserSearchViewHolder(view : View) : RecyclerView.ViewHolder(view){
     val photo: ImageView = view.findViewById(R.id.user_photo)
     val username: EmojiTextView = view.findViewById(R.id.user_username)
     val status: EmojiTextView = view.findViewById(R.id.user_status)
+
+    init {
+        view.setOnClickListener {
+            photo.context.toast("Loading chat...")
+        }
+    }
 }
