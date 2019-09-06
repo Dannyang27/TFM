249bcfc90832988eb5fe2f175900807a0f8bfb07
ledannyang <danny27995@gmail.com>
2019-09-06 13:08:18 +0200

    UserProfileActivity layout changes have been made

--- app/src/main/java/com/example/tfm/activity/UserProfileActivity.kt
+++ app/src/main/java/com/example/tfm/activity/UserProfileActivity.kt
@@ -1,11 +1,17 @@
 package com.example.tfm.activity
 
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.tfm.R
+import org.jetbrains.anko.toast
 
 class UserProfileActivity : AppCompatActivity() {
+
+    private lateinit var username: LinearLayout
+    private lateinit var status: LinearLayout
+    private lateinit var email: LinearLayout
 
     private lateinit var toolbar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +19,23 @@ class UserProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_user_profile)
 
         toolbar = findViewById(R.id.profile_toolbar)
-        toolbar.title = ""
+        toolbar.title = getString(R.string.profile)
+
+        username = findViewById(R.id.profile_username_layout)
+        status = findViewById(R.id.profile_status_layout)
+        email = findViewById(R.id.profile_email_layout)
+
+        username.setOnClickListener {
+            toast("Changing username")
+        }
+
+        status.setOnClickListener {
+            toast("Changing status")
+        }
+
+        email.setOnClickListener {
+            toast("Changing email")
+        }
 
         displayArrowBack(toolbar)
     }
