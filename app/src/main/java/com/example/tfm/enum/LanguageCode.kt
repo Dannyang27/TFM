35aabb9419b8077b828ff573651eb6ed5e3c21e3
ledannyang <danny27995@gmail.com>
2019-09-10 14:18:33 +0200

    Language Enum addded

---
+++ app/src/main/java/com/example/tfm/enum/LanguageCode.kt
@@ -0,0 +1,23 @@
+package com.example.tfm.enum
+
+enum class LanguageCode constructor(val code: String) {
+    ARABIC("ar"),
+    DANISH("da"),
+    DUTCH("nl"),
+    CATALAN("ca"),
+    CHINESE("zh"),
+    ENGLISH("en"),
+    FRENCH("fr"),
+    GERMAN("de"),
+    GREEK("el"),
+    HEBREW("he"),
+    INDIAN("hi"),
+    ITALIAN("it"),
+    JAPANESE("ja"),
+    KOREAN("ko"),
+    NORWEGIAN("no"),
+    POLISH("pl"),
+    PORTUGUESE("pt"),
+    RUSSIAN("ru"),
+    SPANISH("es"),
+}
